7ff4a4b52ecac69552856fd88ab36d3937278326
Ray Brooks <ray.userinterface@gmail.com>
2023-11-18 01:20:27 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,54 @@
+FROM --platform=linux/arm64 node:lts-bullseye-slim as node
+WORKDIR /app
+
+RUN apt update -y
+RUN apt install curl -y
+
+RUN curl -fsSL https://deb.nodesource.com/setup_21.x | bash -
+
+RUN rm -rf /var/lib/apt/lists/*
+
+COPY package.json .
+COPY main.js .
+
+RUN yarn install
+
+FROM node:lts-bullseye-slim
+WORKDIR /app
+
+ARG USER="viewer"
+ARG PASSWORD="default"
+ARG HOST="http://localhost"
+ARG PLAYLIST_ID="0"
+ARG DISPLAY=":0"
+
+ENV USER=${USER}
+ENV PASSWORD=${PASSWORD}
+ENV HOST=${HOST}
+ENV PLAYLIST_ID=${PLAYLIST_ID}
+ENV DISPLAY=${DISPLAY}
+
+RUN echo "USER=${USER}"
+RUN echo "PASSWORD=${PASSWORD}"
+RUN echo "HOST=${HOST}"
+RUN echo "PLAYLIST_ID=${PLAYLIST_ID}"
+RUN echo "DISPLAY=${DISPLAY}"
+
+ARG UID=1001
+ENV UID=$UID
+RUN useradd -m --uid $UID -g users -G plugdev,video,audio user
+
+RUN apt update -y
+RUN apt install curl procps libglib2.0-dev libnss3-dev libatk1.0-dev libatk-bridge2.0-dev libatk1.0-dev libgtk2.0-dev \
+  libdbus-1-dev libx11-dev libx11-xcb-dev  libxcb1-dev libxi-dev \
+  libxcursor-dev libxdamage-dev libxrandr-dev libxcomposite-dev libxext-dev \
+  libxfixes-dev libxrender-dev libxtst-dev libxss-dev libgconf2-dev \
+  libdrm2 libgtk-3-0 libgbm1 libasound2 libxss1 -y
+
+RUN curl -fsSL https://deb.nodesource.com/setup_21.x | bash -
+
+COPY --chown=user:users --from=node /app .
+
+USER user
+
+CMD npx electron --no-sandbox ./main.js
---
+++ main.js
@@ -0,0 +1,68 @@
+const { env } = process;
+const { app, session, BrowserWindow } = require("electron");
+const axios = require("axios");
+
+const user = env.USER || "viewer";
+const password = env.PASSWORD || "default";
+const host = env.HOST || "http://localhost";
+const playlistId = env.PLAYLIST_ID || "eb4348b0-894e-4f36-b286-b99bb674576c";
+
+async function createWindow() {
+  session.defaultSession.clearStorageData();
+
+  const mainWindow = new BrowserWindow({
+    x: 0,
+    y: 0,
+    width: 1920,
+    height: 1080,
+    kiosk: true,
+    webPreferences: {
+      devTools: false,
+    },
+  });
+
+  try {
+    const credentials = { user, password };
+
+    const res = await axios.post(`${host}/login`, credentials, {
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": JSON.stringify(credentials).length,
+      },
+    });
+
+    res.headers["set-cookie"].forEach((cookie) => {
+      const fields = cookie.split("; ");
+      const name = fields[0].split("=")[0];
+      const value = fields[0].split("=")[1];
+      const path = fields[1].split("=")[1];
+      const maxAge = fields[2].split("=")[1];
+      session.defaultSession.cookies.set({
+        url: `${host}`,
+        name,
+        value,
+        path,
+      });
+    });
+
+    mainWindow.webContents.loadURL(
+      `${host}/playlists/play/${playlistId}?kiosk`
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
